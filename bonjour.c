d59a5fa4c8c4822e24b3da407c5cfb2312b4ae19
Jonathan N <ntoula.jonathan@gmail.com>
2022-08-13 22:55:22 +0200

    ajout menu de sélection de langues

--- bonjour.c
+++ bonjour.c
@@ -2,7 +2,7 @@
 # Nom ......... : bonjour.c
 # Rôle ........ : Affiche un message de bienvenue à l'écran dans plusieurs langues
 # Auteur ...... : Jonathan Ntoula
-# Version ..... : V0.1 du 13/08/2022
+# Version ..... : V0.4 du 13/08/2022
 # Licence ..... : GNU GPL
 # Compilation . : gcc -Wall bonjour.c -o bonjour
 #******************************************************* */
@@ -12,8 +12,29 @@
 typedef char * str; // commodité
 
 int main(int argc, char const *argv[])
-{	printf("Bonjour et bienvenue dans ce programme inutile\n") ;
-	printf("Hello, welcome to this pointless program\n") ;
-	printf("Hallo und willkommen in diesem nutzlosen Programm\n");
-	printf("Hola y bienvenido a este inútil programa\n");
+{	int key = 0;
+	
+	printf("Sélectionnez votre langue / Select your language :\n");
+	printf("1 : Je parle français\n2 : I speak English\n3 : I spreche Deutsch\n4 : Hablo español\n\n");
+	printf("Votre choix / Your choice (1, 2, 3 ou/or/oder/o 4) : ");
+	scanf("%d", &key);
+	
+	switch (key)
+	{
+	case 1 :
+		printf("Bonjour et bienvenue dans ce programme inutile\n");
+		break;
+	case 2 :
+		printf("Hello, welcome to this pointless program\n") ;
+		break;
+	case 3 :
+		printf("Hallo und willkommen in diesem nutzlosen Programm\n");
+		break;
+	case 4 :
+		printf("Hola y bienvenido a este inútil programa\n");
+		break;
+	default: //alphabet phonétique international
+		printf("Bravo India Echo November Victor Echo November Uniform Echo\n");
+		break;
+	}
 	return 0; }
